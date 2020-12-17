010f3d674287dcfd61f35838a606c83f9af59dc7
shota_1904 <shota1904@hotmail.com>
2020-12-17 22:17:05 +0100

    change

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//function for the "back-Button"
+function goBack(){
+				window.history.back();
+			}
+
+
+//function for timer
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0;
+			document.getElementById('time').innerHTML = "Fertig!";
+        }
+    }, 1000);
+}
+
+//sets 30 seconds for timer
+function thirtySecondsTimer() {
+    var thirtySeconds = 30,
+        display = document.querySelector('#time');
+    startTimer(thirtySeconds, display);
+}
